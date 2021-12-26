3015a8eae0fbd8236d63b1d1c7ad778ee89e77d6
esanchep <esanchezp85@gmail.com>
2021-12-26 18:50:41 +0100

    Add routing example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -18,8 +18,63 @@ function App() {
         >
           Learn React
         </a>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="about">
+              <Route index element={<About />} />
+              <Route path="test1" element={<Test1 />} />
+              <Route path="test2" element={<Test2 />} />
+            </Route>
+          </Route>
+        </Routes>
       </header>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>You are in HOME</h2>
+      </main>
+      <nav>
+        Home | <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>You are in ABOUT</h2>
+      </main>
+      <nav>
+        <Link to="/">Home</Link> | About
+        <Outlet />
+      </nav>
+    </>
+  );
+}
+
+function Test1() {
+  return (
+    <>
+      This is TEST 1
+      <Link to="../test2">To TEST2</Link>
+    </>
+  );
+}
+
+function Test2() {
+  return (
+    <>
+      This is TEST 2
+      <Link to="../test1">To TEST1</Link>
+    </>
   );
 }
 
